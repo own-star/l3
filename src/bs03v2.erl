3f952a722707ea38da46a820ac842a350789d0af
Taras J. Honcharuk <taras.honcharuk@gmail.com>
2016-02-23 22:21:41 +0200

    Fix bs03v2

--- src/bs03v2.erl
+++ src/bs03v2.erl
@@ -4,5 +4,11 @@
 split(Bin,Del) ->
 	split(Bin,Del,Del,<<>>).
 
+split(<<H,Rem/binary>>,[H|[]],Del,Acc) ->
+	[Acc|split(Rem,Del,Del,<<>>)];
 split(<<H,Rem/binary>>,[H|T],Del,Acc) ->
-	split(Rem,T,Del,Acc).
+	split(Rem,T,Del,Acc);
+split(<<X,Rem/binary>>,Del,Del,Acc) ->
+	split(Rem,Del,Del,<<Acc/binary,X>>);
+split(<<>>,_,_,Acc) ->
+	[Acc].
