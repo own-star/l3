fd6d297f36e2251bb3ff8bdce842532c715836cb
own-star <taras.honcharuk@gmail.com>
2016-02-26 11:52:01 +0200

    Add xml_parser with comments and sigle quoted attributes

--- ebin/l3.app
+++ ebin/l3.app
@@ -1,7 +1,7 @@
 {application, l3, [
 	{description, "New project"},
 	{vsn, "0.0.1"},
-	{modules, ['bs01','bs02','bs03','bs03v1','bs03v2','bs04']},
+	{modules, ['bs01','bs02','bs03','bs03v1','bs03v2','bs04','xml_parser']},
 	{registered, []},
 	{applications, [kernel,stdlib]}
 ]}.
---
+++ src/xml_parser.erl
@@ -0,0 +1,54 @@
+-module(xml_parser).
+-export([decode_xml/1]).
+
+decode_xml(Bin) ->
+	decode_xml(Bin,[],[]).
+
+decode_xml(Bin,Stack,AStack) ->
+	case Bin of
+		<<"</",Rest/binary>> ->
+			stop_tag(Rest,<<>>,Stack,AStack);
+		<<"<!--",Rest/binary>> ->
+			start_comment(Rest,Stack,AStack);
+		<<"<",Rest/binary>> ->
+			start_tag(Rest,<<>>,Stack,AStack);
+		<<>> -> []
+	end.
+
+stop_tag(<<">",Rest/binary>>,Acc,[Acc|Stack],AStack) ->
+	decode_xml(Rest,Stack,AStack);
+stop_tag(<<X,Rest/binary>>,Acc,Stack,AStack) ->
+	stop_tag(Rest,<<Acc/binary,X>>,Stack,AStack).
+
+start_tag(<<"</",Rest/binary>>,Acc,[Tag|Stack],AStack) ->
+	[{Tag, get_attr(Tag,AStack), [Acc]}|stop_tag(Rest,<<>>,[Tag|Stack],AStack)];
+start_tag(<<"<",Rest/binary>>,<<>>,[Tag|Stack],AStack) ->
+	{Tag, get_attr(Tag,AStack), decode_xml(<<"<",Rest/binary>>,[Tag|Stack],AStack)};
+start_tag(<<" />",Rest/binary>>,Acc,Stack,AStack) ->
+	[{Acc, get_attr(Acc,AStack), []}|decode_xml(Rest,Stack,AStack)];
+start_tag(<<" ",Rest/binary>>,Acc,Stack,AStack) ->
+	start_attr(Rest,Acc,Stack,AStack,<<>>);
+start_tag(<<">",Rest/binary>>,Acc,Stack,AStack) ->
+	start_tag(Rest,<<>>,[Acc|Stack],AStack);
+start_tag(<<X,Rest/binary>>,Acc,Stack,AStack) ->
+	start_tag(Rest,<<Acc/binary,X>>,Stack,AStack).
+
+start_comment(<<"-->",Rest/binary>>,Stack,AStack) ->
+	decode_xml(Rest,Stack,AStack);
+start_comment(<<_,Rest/binary>>,Stack,AStack) ->
+	start_comment(Rest,Stack,AStack).
+
+
+start_attr(<<" />",_/binary>>=Rest,Tag,Stack,AStack,_) ->
+	start_tag(Rest,Tag,Stack,AStack);
+start_attr(<<">",_/binary>>=Rest,Tag,Stack,AStack,_) ->
+	start_tag(Rest,Tag,Stack,AStack);
+start_attr(<<"'",Rest/binary>>,Tag,Stack,[H|AStack],Acc) ->
+	start_attr(Rest,Tag,Stack,[{Tag, H, Acc}|AStack],<<>>);
+start_attr(<<"='",Rest/binary>>,Tag,Stack,AStack,Acc) ->
+	start_attr(Rest,Tag,Stack,[Acc|AStack],<<>>);
+start_attr(<<X,Rest/binary>>,Tag,Stack,AStack,Acc) ->
+	start_attr(Rest,Tag,Stack,AStack,<<Acc/binary,X>>).
+
+get_attr(Tag,Stack) ->
+	[{Key, Val} || {T, Key, Val} <- Stack, T =:= Tag].
